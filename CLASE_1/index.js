0b3a9dc5d5a2821b85e3529d3c3ed616d5da1dbf
domm1828 <67112778+domm1828@users.noreply.github.com>
2023-07-17 17:12:42 -0600

    Add CLASE UNO

---
+++ CLASE_1/index.js
@@ -0,0 +1,15 @@
+/** TRES MANERA DE DECLARAR UNA VARIABLE EN JS */
+//variable
+var uno=12;
+//variable de bloques { }
+let dos ='12'; 
+// variables const
+const tres= false;
+
+uno++;
+uno++;
+//typeof
+console.log(typeof dos);
+console.log(typeof uno);
+console.log(typeof tres);
+console.log(uno-=x);
