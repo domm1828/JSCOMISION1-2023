f1234e0afa2d75d1d2f4287b24c30a88ba9ee8fd
domm1828 <67112778+domm1828@users.noreply.github.com>
2023-09-06 16:57:13 -0600

    Add Class React

---
+++ CLASE_REACT/storevirtual/app/about/contactos/page.tsx
@@ -0,0 +1,8 @@
+const dashboardPage = ()=>{
+return(
+    <div className="p-8">
+        <h1 className="text-4xl py-4">Dashboard Page contactos</h1>
+    </div>
+);
+}
+export default dashboardPage;
---
+++ CLASE_REACT/storevirtual/app/contact/page.tsx
@@ -0,0 +1,8 @@
+const contactPage = ()=>{
+return(
+    <div className="p-8">
+        <h1 className="text-4xl py-4">Contactos Page</h1>
+    </div>
+);
+}
+export default contactPage;
